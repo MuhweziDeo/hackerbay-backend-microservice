e59b87d3873937c9464774ea52b71fc94f4de7c1
muhwezideo <aggrey256@gmail.com>
2020-01-15 11:16:57 +0300

    add documentation

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import express from "express";
 import morgan from "morgan";
 import { errors } from "celebrate";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 import { apiPrefix } from "./config";
 import authRoutes from "./authentication";
 import patchRoutes from "./json-patch";
 import thumbnailRoutes from "./thumbnail";
 
+const swaggerDocument = YAML.load("./swagger.yml");
 const app = express();
 
 app.use(express.json());
 app.use(morgan("combined"));
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Routes
 app.use(`${apiPrefix}/auth`, authRoutes);
 app.use(`${apiPrefix}/patch`, patchRoutes);
